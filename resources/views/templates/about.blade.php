a2bfde7a76c8be5184f7f0713afabe5e7f43e1b7
Nathan Van Dyck <vandyck.nathan@gmail.com>
2020-05-13 11:29:44 +0200

    quick services<about< page home

--- resources/views/templates/about.blade.php
+++ resources/views/templates/about.blade.php
@@ -5,41 +5,53 @@
   <!-- services rapides -->
   <div class="card-section">
     <div class="container">
-      <div class="row">
-
-        <!-- single card -->
-        <div class="col-md-4 col-sm-6">
-          <div class="lab-card">
-            <div class="icon">
-              <i class="flaticon-023-flask"></i>
+      <div class="row d-flex justify-content-center">
+        @if ($services_chunks->isEmpty())
+          <!-- single card -->
+          <div class="col-md-4 col-sm-6">
+            <div class="lab-card">
+              <div class="icon">
+                <i class="flaticon-023-flask"></i>
+              </div>
+              <h2>Get in the lab</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
             </div>
-            <h2>Get in the lab</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
           </div>
-        </div>
 
-        <!-- single card -->
-        <div class="col-md-4 col-sm-6">
-          <div class="lab-card">
-            <div class="icon">
-              <i class="flaticon-011-compass"></i>
+          <!-- single card -->
+          <div class="col-md-4 col-sm-6">
+            <div class="lab-card">
+              <div class="icon">
+                <i class="flaticon-011-compass"></i>
+              </div>
+              <h2>Projects online</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
             </div>
-            <h2>Projects online</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
           </div>
-        </div>
 
-        <!-- single card -->
-        <div class="col-md-4 col-sm-12">
-          <div class="lab-card">
-            <div class="icon">
-              <i class="flaticon-037-idea"></i>
+          <!-- single card -->
+          <div class="col-md-4 col-sm-12">
+            <div class="lab-card">
+              <div class="icon">
+                <i class="flaticon-037-idea"></i>
+              </div>
+              <h2>SMART MARKETING</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
             </div>
-            <h2>SMART MARKETING</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
           </div>
-        </div>
-
+        @else
+          @foreach ($services_chunks->first()->chunk(3)->first() as $quick_service)
+            <div class="col-md-4 col-sm-12">
+              <div class="lab-card">
+                <div class="icon">
+                  <i class="{{$quick_service->icon}}"></i>
+                </div>
+                <h2>{{$quick_service->title}}</h2>
+                <p>{{$quick_service->description}}</p>
+              </div>
+            </div>
+          @endforeach
+        @endif
       </div>
     </div>
   </div>
